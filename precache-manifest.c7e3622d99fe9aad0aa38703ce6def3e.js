98d926ff3e11a2663597211b22412ceee40293db
TanyawatVittayapalotai <hamzaabamboo@gmail.com>
2020-10-15 17:32:52 +0000

    "gh-pages"

---
+++ precache-manifest.c7e3622d99fe9aad0aa38703ce6def3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13d2fc91157a31766e7",
+    "url": "/seiyuu-navigator/css/app.c442ce06.css"
+  },
+  {
+    "revision": "e97e4fac7518eb820e3e",
+    "url": "/seiyuu-navigator/css/chunk-vendors.99d797fd.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/seiyuu-navigator/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "6fcaf22dba69a8c1587c5e8107dd1317",
+    "url": "/seiyuu-navigator/index.html"
+  },
+  {
+    "revision": "26f0c77b2acfaaafb52d",
+    "url": "/seiyuu-navigator/js/about.1f709467.js"
+  },
+  {
+    "revision": "d13d2fc91157a31766e7",
+    "url": "/seiyuu-navigator/js/app.77f3a90e.js"
+  },
+  {
+    "revision": "e97e4fac7518eb820e3e",
+    "url": "/seiyuu-navigator/js/chunk-vendors.5df1d005.js"
+  },
+  {
+    "revision": "f074305ae0472bc3c29aea7a87c49e4d",
+    "url": "/seiyuu-navigator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/seiyuu-navigator/robots.txt"
+  }
+]);
